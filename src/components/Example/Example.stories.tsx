a1745a22df502870e616f730a4105b93c7b61101
Camila Rondinini <crondinini@gmail.com>
2021-11-28 02:00:54 +0100

    review(@Dhaiwat10): remove example

--- src/components/Example/Example.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { Example } from './Example';
-
-storiesOf('Example', module)
-  .add('Default', () => <Example>Button</Example>)
-  .add('Active', () => <Example active>Button</Example>)
-  .add('Disabled', () => <Example disabled>Button</Example>);
