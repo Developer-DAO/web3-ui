e3f921ee7e026a86a9030c7fdb43e750ca056fb7
Erik Ritter <erik.ritter@airbnb.com>
2021-11-29 11:56:57 -0800

    Add test setup for monorepo and example test (#25)
    * Add example test and setupTests.ts
    
    * Update package.json
    
    * Update package.json
    
    * Update Address.test.tsx

---
+++ packages/components/src/components/Address/Address.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Address } from './Address';
+
+describe('Address', () => {
+  it('renders without throwing', () => {
+    const { container } = render(<Address value="taylorswift.eth" />);
+    expect(container).toBeInTheDocument();
+  });
+});
