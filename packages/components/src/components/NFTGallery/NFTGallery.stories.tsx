23b61d12030bff9e03b618c946ce89bca938e589
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-12-03 12:51:20 +0530

    feat: add ENS support for NFTGallery

--- packages/components/src/components/NFTGallery/NFTGallery.stories.tsx
+++ packages/components/src/components/NFTGallery/NFTGallery.stories.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
 import { NFTGallery } from '.';
 
-export default {
-  title: 'Components/NFTGallery',
-  component: NFTGallery,
-};
+storiesOf('NFTGallery', module)
+  .add('Renders a list of nfts owned by an account', () => (
+    <NFTGallery address='0x1A16c87927570239FECD343ad2654fD81682725e' />
+  ))
+  .add('Renders a list of NFTs owned by an ENS address', () => {
+    const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
+
+    useEffect(() => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
+    }, []);
+
+    if (!provider) {
+      return <>Loading...</>;
+    }
+
+    return <NFTGallery address='dhaiwat.eth' web3Provider={provider} />;
+  });
 
 export const Default = () => <NFTGallery address='0x1A16c87927570239FECD343ad2654fD81682725e' />;
 
--- packages/components/src/components/NFTGallery/NFTGallery.tsx
+++ packages/components/src/components/NFTGallery/NFTGallery.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import fetch from 'cross-fetch';
-
+import { ethers } from 'ethers';
 import { VStack, Heading, Grid, Alert, AlertIcon } from '@chakra-ui/react';
 import { NFT } from '../NFT';
 
@@ -13,6 +13,7 @@ export interface NFTGalleryProps {
    * The number of columns in the grid
    */
   gridWidth?: number;
+  web3Provider?: ethers.providers.Web3Provider;
 }
 
 export interface OpenSeaAsset {
@@ -29,21 +30,31 @@ export interface OpenSeaAsset {
  * Component to display a grid of NFTs owned by an address. It uses the OpenSea API to fetch
  * the NFTs.
  */
-export const NFTGallery = ({ address, gridWidth = 4 }: NFTGalleryProps) => {
+export const NFTGallery = ({ address, gridWidth = 4, web3Provider }: NFTGalleryProps) => {
   const [nfts, setNfts] = React.useState<OpenSeaAsset[]>([]);
   const [errorMessage, setErrorMessage] = React.useState();
 
   useEffect(() => {
-    fetch(`https://api.opensea.io/api/v1/assets?owner=${address}`)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`OpenSea request failed with status: ${res.status}.`);
+    async function exec() {
+      let resolvedAddress: string | null = address;
+      if (address.endsWith('.eth')) {
+        if (!web3Provider) {
+          return console.error('Please provide a web3 provider');
         }
-        return res.json();
-      })
-      .then((data) => setNfts(data.assets))
-      .catch((err) => setErrorMessage(err.message));
-  }, [address]);
+        resolvedAddress = await web3Provider.resolveName(address);
+      }
+      fetch(`https://api.opensea.io/api/v1/assets?owner=${resolvedAddress}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error(`OpenSea request failed with status: ${res.status}.`);
+          }
+          return res.json();
+        })
+        .then((data) => setNfts(data.assets))
+        .catch((err) => setErrorMessage(err.message));
+    }
+    exec();
+  }, [address, web3Provider]);
 
   return (
     <VStack>
