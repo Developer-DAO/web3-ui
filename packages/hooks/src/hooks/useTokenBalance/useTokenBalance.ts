07e85461dacf007042e3d842590dfff99db8f084
Alex <alex.plutta@googlemail.com>
2021-12-07 05:17:27 +0100

    feat(hooks): add `useTokenBalance` (#69)
    * added ustTokenBalance hook
    
    * added error handling
    
    * export useTokenBalance from /hooks directory
    
    * . change hasError to errorMsg
    . rename ERC20 to ERC20.json
    . change name of args of useTokenBalance.ts
    
    * . use constants in stories
    . use rinkeby instead of mainnet
    
    * change order of hook's return values
    
    * Refactor and fix useTokenBalance
    
    Co-authored-by: Dhaiwat Pandya <dhaiwatpandya@gmail.com>

---
+++ packages/hooks/src/hooks/useTokenBalance/useTokenBalance.ts
@@ -0,0 +1,51 @@
+import { ethers } from 'ethers';
+import { useContext, useEffect, useState } from 'react';
+import { Web3Context } from '../..';
+import ERC20ABI from './ERC20ABI.json';
+import { BigNumber } from '@ethersproject/bignumber';
+
+interface Props {
+  tokenAddress: string;
+  accountAddress: string;
+}
+
+export function useTokenBalance({ tokenAddress, accountAddress }: Props) {
+  const context = useContext(Web3Context);
+  const provider = context?.provider;
+
+  const [balance, setBalance] = useState<BigNumber>();
+  const [decimals, setDecimals] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const getBalance = async () => {
+    const contract = new ethers.Contract(tokenAddress, ERC20ABI, provider!);
+    const balance = await contract.balanceOf(accountAddress);
+    const decimals = await contract.decimals();
+    setBalance(balance);
+    setDecimals(decimals);
+  };
+
+  useEffect(() => {
+    if (tokenAddress && accountAddress && provider) {
+      setError(false);
+      setLoading(true);
+      try {
+        getBalance();
+      } catch (error) {
+        console.error(error);
+        setError(true);
+      }
+      setLoading(false);
+    }
+  }, [tokenAddress, accountAddress]);
+
+  return {
+    balance: balance?.toString(), // The balance in wei
+    loading,
+    error,
+    decimals,
+    formattedBalance: balance && ethers.utils.formatUnits(balance, decimals), // The balance in ethers eg. 0.01 ETH, 20 GTC, etc.
+    balanceInBigNumber: balance,
+  };
+}
---
+++ packages/hooks/src/stories/UseTokenBalance.stories.tsx
@@ -0,0 +1,40 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { Button } from '@chakra-ui/react';
+import { Provider, useTokenBalance, useWallet } from '..';
+
+const GTC_ADDRESS = '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f';
+
+const Default = () => {
+  const { connection, connectWallet, disconnectWallet, connected } = useWallet();
+  const { balance, formattedBalance, loading, error } = useTokenBalance({
+    tokenAddress: GTC_ADDRESS,
+    accountAddress: connection.userAddress!, // you can test using 0x0ED6Cec17F860fb54E21D154b49DAEFd9Ca04106
+  });
+
+  if (connected) {
+    return (
+      <div>
+        <Button onClick={disconnectWallet}>Disconnect wallet</Button>
+        <p>{connection.ens || connection.userAddress}</p>
+        {error ? (
+          <p>Error occured while trying to fetch balance.</p>
+        ) : loading ? (
+          <p>Loading...</p>
+        ) : (
+          <p>
+            GTC balance: {balance} wei, {formattedBalance} GTC
+          </p>
+        )}
+      </div>
+    );
+  }
+
+  return <button onClick={connectWallet}>Connect Wallet</button>;
+};
+
+storiesOf('UseTokenBalance', module).add('Default', () => (
+  <Provider network='mainnet'>
+    <Default />
+  </Provider>
+));
