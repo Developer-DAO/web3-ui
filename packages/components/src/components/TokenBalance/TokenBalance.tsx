065c576e30884a8775211c69f782eca3a78a6c51
Alex Plutta <alex.plutta@googlemail.com>
2021-12-12 15:20:55 +0100

    use useTokenBalance hook to determine token balance

--- packages/components/src/components/TokenBalance/TokenBalance.tsx
+++ packages/components/src/components/TokenBalance/TokenBalance.tsx
@@ -1,71 +1,56 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import { Box, Flex, Heading } from '@chakra-ui/react';
-import { Logo, TokenLogo } from './components/Logo';
-import { useWallet, Web3Context } from '@web3-ui/hooks/src';
-import { ethers } from 'ethers';
-import ERC20 from './erc.json';
+import { TokenLogo } from './components/Logo';
+import { useTokenBalance } from '@web3-ui/hooks/src';
 
 export interface TokenBalanceProps {
   name: string;
   symbol: string;
-  address: string;
-  owner: string;
+  tokenAddress: string;
+  accountAddress: string;
   value: number;
   imgUrl?: string | undefined;
 }
 
 export const TokenBalance = (props: TokenBalanceProps) => {
-  const { name, symbol, address, owner, value, imgUrl } = props;
+  const { name, symbol, tokenAddress, accountAddress, value, imgUrl } = props;
   const Headline = (name: string) => (
     <Heading as='h3' size='sm'>
       {name}
     </Heading>
   );
 
-  const { connectWallet } = useWallet();
+  const { balance, decimals } = useTokenBalance({ tokenAddress, accountAddress });
 
-  const { provider } = useContext(Web3Context);
-
-  const [balance, setBalance] = useState(0);
-
-  const getBalance = async () => {
-    if (provider === undefined) {
-      return;
+  const calcBalance = () => {
+    if (balance === undefined || decimals === undefined) {
+      return 0;
     }
-    const contract = new ethers.Contract(address, ERC20, provider);
-    const balanceFuture = contract.balanceOf(owner);
-    const decimalsFuture = contract.decimals();
-
-    const [resolvedBalance, resolvedDecimals] = await Promise.all([balanceFuture, decimalsFuture]);
-
-    const balanceAsNumber = Number.parseInt(resolvedBalance);
-    const displayBalance = (balanceAsNumber / 10 ** resolvedDecimals).toFixed(3);
-
-    setBalance(displayBalance);
+    return Number.parseFloat((Number.parseInt(balance) / 10 ** decimals).toFixed(3));
   };
 
-  useEffect(() => {
-    (async () => {
-      await getBalance();
-    })();
-  }, []);
+  const calcValue = () => {
+    const balance = calcBalance();
+
+    return (balance * value).toFixed(3);
+  };
 
   return (
     <Flex borderRadius='lg' borderWidth='1px' overflow='hidden' px='4' py='2'>
       <Box w='50px' pr='4'>
-        <TokenLogo tokenContractAddress={address} />
+        <TokenLogo tokenContractAddress={tokenAddress} />
       </Box>
-      <Box flex='3'>
+      <Box flex='2'>
         {Headline(symbol)}
         <p>{name}</p>
       </Box>
       <Box flex='1'>
         {Headline('Balance')}
-        <p>{balance}</p>
+        <p>{calcBalance()}</p>
       </Box>
       <Box flex='1'>
         {Headline('Value')}
-        <p>{(balance * value).toFixed(3)}</p>
+        <p>{calcValue()}</p>
       </Box>
     </Flex>
   );
