1fa5469cceb0e1d946016c01b4e5829c35af492f
Jose L. Velez <48886275+joselvelez@users.noreply.github.com>
2021-12-12 12:16:38 -0600

    Update packages/components/src/components/Address/Address.test.tsx
    Co-authored-by: Erik Ritter <erik.ritter@airbnb.com>

--- packages/components/src/components/Address/Address.test.tsx
+++ packages/components/src/components/Address/Address.test.tsx
@@ -5,7 +5,7 @@ import { Address } from './Address';
 
 describe('Address', () => {
   it('renders without throwing', () => {
-    const { container } = render(<Address value='taylorswift.eth' shortened={false} />);
+    const { container } = render(<Address value='taylorswift.eth' />);
     expect(container).toBeInTheDocument();
   });
 });
