34738c40519fa75e094e88c82683e150e47e217a
Maxime Bonhomme <eslesq@gmail.com>
2021-12-13 11:27:39 +0000

    86-address-component-copiable: Update Address component to accept copiable boolean prop - make sure we copy input value to clipboard when clicking on input - display icons at far right of the input when copiable is set to true as well as a feedback check mark when copied

--- packages/components/src/components/Address/Address.tsx
+++ packages/components/src/components/Address/Address.tsx
@@ -1,16 +1,67 @@
-import { Input } from '@chakra-ui/react';
-import React from 'react';
+import {
+  FormControl,
+  FormErrorMessage,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from '@chakra-ui/react';
+import { CopyIcon, CheckIcon } from '@chakra-ui/icons';
+import React, { useState } from 'react';
 
 export interface AddressProps {
   /**
    * The address to display
    */
   value: string;
+  /**
+   * Wheter the address can be copied or not
+   */
+  copiable?: boolean;
+}
+
+interface EventTarget {
+  value: string;
+}
+
+interface SyntheticEvent {
+  currentTarget: EventTarget;
 }
 
 /**
  * A component to display an address
  */
-export const Address: React.FC<AddressProps> = ({ value }) => {
-  return <Input value={value} />;
+export const Address: React.FC<AddressProps> = ({ value, copiable = false }) => {
+  const [error, setError] = useState<null | string>(null);
+  const [copied, setCopied] = useState<boolean>(false);
+
+  const handleClick = async (event: SyntheticEvent): Promise<void> => {
+    const value = event.currentTarget.value as string;
+
+    setError(null);
+    setCopied(false);
+
+    if (copiable && value) {
+      try {
+        await navigator.clipboard.writeText(value);
+        setCopied(true);
+      } catch (error) {
+        setError(error as string);
+      }
+    }
+  };
+
+  return (
+    <FormControl isInvalid={!!error}>
+      <InputGroup>
+        {copiable && (
+          <InputRightElement
+            pointerEvents='none'
+            children={copied ? <CheckIcon color='green.500' /> : <CopyIcon color='gray.300' />}
+          />
+        )}
+        <Input value={value} onClick={handleClick} />
+      </InputGroup>
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
 };
