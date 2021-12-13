12e4accd45606ea509e7d48ce326fc043280c047
joselvelez <joselvelez@protonmail.com>
2021-12-13 09:37:38 -0600

    add test for shortened address

--- packages/components/src/components/Address/Address.test.tsx
+++ packages/components/src/components/Address/Address.test.tsx
@@ -5,7 +5,15 @@ import { Address } from './Address';
 
 describe('Address', () => {
   it('renders without throwing', () => {
-    const { container } = render(<Address value='taylorswift.eth' />);
+    const { container } = render(<Address value='taylorswift.eth' shortened={false} />);
     expect(container).toBeInTheDocument();
+  });
+});
+
+describe('Shortened Address', () => {
+  it('check the length of the address', () => {
+    const { container } = render(<Address value='0x00000000000000' shortened />);
+    const addressInput = container.querySelector('input') as HTMLInputElement;
+    expect(addressInput).toHaveValue('0x00...0000');
   });
 });
