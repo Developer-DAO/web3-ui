42a94e65a67b0079b6e325211b573e2d8db42065
Maxime Bonhomme <eslesq@gmail.com>
2021-12-13 00:51:21 +0000

    86-address-component-copiable: Add new story for the Address component with copiable set to true - add tests to make sure input is copiable

--- packages/components/src/components/Address/Address.test.tsx
+++ packages/components/src/components/Address/Address.test.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { jest } from '@jest/globals';
+import { render, fireEvent, waitFor } from '@testing-library/react';
 
 import { Address } from './Address';
 
+Object.assign(navigator, {
+  clipboard: {
+    writeText: () => {},
+  },
+});
+
 describe('Address', () => {
   it('renders without throwing', () => {
-    const { container } = render(<Address value="taylorswift.eth" />);
+    const { container } = render(<Address value='taylorswift.eth' />);
     expect(container).toBeInTheDocument();
+  });
+});
+
+describe('Address copiable prop true', () => {
+  it('renders without throwing', () => {
+    const { container } = render(<Address copiable value='taylorswift.eth' />);
+    expect(container).toBeInTheDocument();
+  });
+
+  it('renders with icon', () => {
+    const { container } = render(<Address copiable value='taylorswift.eth' />);
+    const svg = container.querySelector('svg') as SVGElement;
+
+    expect(svg).toBeInTheDocument();
+  });
+
+  it('uses writeText from Clipboard API', async () => {
+    const { container } = render(<Address copiable value='taylorswift.eth' />);
+    const input = container.querySelector('input') as HTMLElement;
+
+    jest.spyOn(navigator.clipboard, 'writeText');
+
+    fireEvent.click(input);
+
+    await waitFor(() => {
+      expect(navigator.clipboard.writeText).toHaveBeenCalledWith('taylorswift.eth');
+    });
   });
 });
