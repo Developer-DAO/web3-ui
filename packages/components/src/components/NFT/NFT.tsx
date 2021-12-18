13dd8e29f13142fe8e624f211d4ff576b93d26ba
erik_ritter <erik.ritter@airbnb.com>
2021-12-17 22:09:13 -0800

    Fix NFT sizes and add docs

--- packages/components/src/components/NFT/NFT.tsx
+++ packages/components/src/components/NFT/NFT.tsx
@@ -14,8 +14,18 @@ import {
 import fetch from 'cross-fetch';
 
 export interface NFTProps {
+  /**
+   * The address of the NFT contract.
+   */
   contractAddress: string;
+  /**
+   * The id of the NFT.
+   */
   tokenId: string;
+  /**
+   * The size of the NFT card.
+   */
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }
 
 export interface NFTData {
@@ -30,7 +40,7 @@ export interface NFTData {
 /**
  * Component to fetch and display NFT data
  */
-export const NFT = ({ contractAddress, tokenId }: NFTProps) => {
+export const NFT = ({ contractAddress, tokenId, size = 'xs' }: NFTProps) => {
   const _isMounted = useRef(true);
   const [nftData, setNftData] = React.useState<NFTData>();
   const [errorMessage, setErrorMessage] = React.useState<string>();
@@ -54,8 +64,12 @@ export const NFT = ({ contractAddress, tokenId }: NFTProps) => {
           animationUrl: data.animation_url,
         });
       }
-    } catch (error: any) {
-      setErrorMessage(error.message);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage('An unknown error occurred');
+      }
     }
   }, [contractAddress, tokenId]);
 
@@ -67,7 +81,7 @@ export const NFT = ({ contractAddress, tokenId }: NFTProps) => {
     };
   }, [contractAddress, tokenId]);
 
-  return <NFTCard data={nftData} errorMessage={errorMessage} />;
+  return <NFTCard data={nftData} errorMessage={errorMessage} size={size} />;
 };
 
 /**
@@ -76,9 +90,11 @@ export const NFT = ({ contractAddress, tokenId }: NFTProps) => {
 export const NFTCard = ({
   data,
   errorMessage = '',
+  size,
 }: {
   data: NFTData | undefined | null;
   errorMessage?: string | undefined;
+  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 }) => {
   const name = data?.name;
   const imageUrl = data?.imageUrl;
@@ -98,23 +114,25 @@ export const NFTCard = ({
   }
 
   return (
-    <Skeleton isLoaded={!!data} maxW='xs' h='md'>
-      <Box maxW='xs' borderRadius='lg' borderWidth='1px' overflow='hidden'>
+    <Skeleton isLoaded={!!data} maxW={size} h='md'>
+      <Box maxW={size} borderRadius='lg' borderWidth='1px' overflow='hidden'>
         {animationUrl ? (
           animationUrl.endsWith('.mp3') ? (
             <VStack>
-              <Image src={imageUrl} alt={displayName} borderRadius='lg' />
+              <Image src={imageUrl} alt={displayName} borderRadius='lg' w={size} />
               <audio src={animationUrl} controls autoPlay muted style={{ borderRadius: '7px' }} />
             </VStack>
           ) : (
-            <video src={animationUrl} controls autoPlay muted />
+            <Flex w={size} h={size} bg='black' justifyContent='center'>
+              <video src={animationUrl} controls autoPlay muted />
+            </Flex>
           )
         ) : (
-          <Image src={imageUrl} alt={displayName} borderRadius='lg' />
+          <Image src={imageUrl} alt={displayName} borderRadius='lg' w={size} />
         )}
         <Box p='6'>
           <Flex alignItems='center' justifyContent='space-between' pb='2'>
-            <Heading as='h3' size='sm'>
+            <Heading as='h3' size='sm' style={{ overflowWrap: 'anywhere' }}>
               {displayName}
             </Heading>
             {assetContractSymbol && <Tag size='sm'>{assetContractSymbol}</Tag>}
--- packages/components/src/components/NFTGallery/NFTGallery.stories.tsx
+++ packages/components/src/components/NFTGallery/NFTGallery.stories.tsx
@@ -12,7 +12,7 @@ export default {
 };
 
 export const nftsOwnedByAnAccount = () => (
-  <NFTGallery address='0x1A16c87927570239FECD343ad2654fD81682725e' />
+  <NFTGallery address='0x1A16c87927570239FECD343ad2654fD81682725e' gridWidth={2} />
 );
 
 export const nftsOwnedByAnENS = () => {
