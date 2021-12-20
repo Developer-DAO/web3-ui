68ef3c826f340115aa7cbe5a0f4d17b54461c148
hone1er <24376928+hone1er@users.noreply.github.com>
2021-12-20 10:20:39 -0800

    added whitespace back to Address.test.tsx to keep history clean. Updated docstring for TokenGate and removed console.log from file.

--- packages/components/src/components/TokenGate/TokenGate.tsx
+++ packages/components/src/components/TokenGate/TokenGate.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 export interface TokenGateProps {
   /**
-   * The provider or signer to fetch the address from the ens
+   * The balance of the required token held in wallet
    */
   walletBalance: number;
   /**
@@ -23,9 +23,7 @@ export const TokenGate: React.FC<TokenGateProps> = ({
   children,
   deniedMessage,
 }) => {
-  console.log(walletBalance);
-
-  // return children within simple container(className optional)
+  // return children within simple container
   return (
     // verify token quantity in wallet is greater than required amount(optional, defaults to 1)
     walletBalance >= requiredQuantity ? (
