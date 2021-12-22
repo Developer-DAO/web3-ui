aa9b2f6cccdcda1d443bacb306d0a83398a19368
hone1er <24376928+hone1er@users.noreply.github.com>
2021-12-22 03:23:51 -0800

    Token gate (#121)
    * Version Packages (#109)
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    * fixed tokenQuantity default
    
    * storybook working with web3hooks
    
    * completed default, web3 hook, and denied access story
    
    * changed deniedMessage from string to ReactNode
    
    * updated requiredQuantity logic in story
    
    * added ERC20ABI from hooks/src/constants
    
    * added ERC20ABI from hooks/src/constants
    
    * changeset added
    
    * removed logic from TokenGate component. Changed stories to work with updated component
    
    * added whitespace back to Address.test.tsx to keep history clean. Updated docstring for TokenGate and removed console.log from file.
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ packages/components/src/components/TokenGate/TokenGate.tsx
@@ -0,0 +1,35 @@
+import React, { ReactNode } from 'react';
+export interface TokenGateProps {
+  /**
+   * The balance of the required token held in wallet
+   */
+  walletBalance: number;
+  /**
+   * The token quantity required to access child component. Default=1
+   */
+  requiredQuantity?: number;
+  /**
+   * Child nodes
+   */
+  children: ReactNode;
+  /**
+   * Optional message if access denied
+   */
+  deniedMessage?: ReactNode;
+}
+export const TokenGate: React.FC<TokenGateProps> = ({
+  walletBalance,
+  requiredQuantity = 1,
+  children,
+  deniedMessage,
+}) => {
+  // return children within simple container
+  return (
+    // verify token quantity in wallet is greater than required amount(optional, defaults to 1)
+    walletBalance >= requiredQuantity ? (
+      <>{children}</>
+    ) : deniedMessage ? (
+      <>{deniedMessage}</>
+    ) : null
+  );
+};
