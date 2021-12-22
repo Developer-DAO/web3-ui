37711d25b1f73cc23545a8a8e1d4b71374e656b9
Brian Eter <brianeter.dev@gmail.com>
2021-12-23 06:11:01 +1100

    Spread ChakraProvider props to Provider Component

--- packages/components/src/components/Provider/Provider.tsx
+++ packages/components/src/components/Provider/Provider.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ChakraProviderProps } from '@chakra-ui/react';
 
 export interface ProviderProps {}
 
-export const Provider: React.FC<ProviderProps> = ({ children }) => {
-  return <ChakraProvider>{children}</ChakraProvider>;
+export const Provider: React.FC<ProviderProps & ChakraProviderProps> = ({ children, ...props }) => {
+  return <ChakraProvider {...props}>{children}</ChakraProvider>;
 };
