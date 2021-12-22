c4001f6baa42f8c34c7bec3f71fbd7d6184eb588
Brian Eter <brianeter.dev@gmail.com>
2021-12-23 09:49:23 +1100

    Implement changes suggested by Wth-Heart for cleaner Interface

--- packages/components/src/components/Provider/Provider.tsx
+++ packages/components/src/components/Provider/Provider.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ChakraProvider, ChakraProviderProps } from '@chakra-ui/react';
 
-export interface ProviderProps {}
+export interface ProviderProps extends ChakraProviderProps {}
 
-export const Provider: React.FC<ProviderProps & ChakraProviderProps> = ({ children, ...props }) => {
+export const Provider: React.FC<ProviderProps> = ({ children, ...props }) => {
   return <ChakraProvider {...props}>{children}</ChakraProvider>;
 };
