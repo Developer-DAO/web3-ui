cccc3e5233853fb1f95f3c48c67c706b513936d7
Greg <gjsyme@gmail.com>
2021-12-26 17:54:53 -0500

    Wrap the useWallet tests in a describe, add comments
    Improve readability of the tests and their output

--- packages/hooks/src/hooks/useWallet.test.tsx
+++ packages/hooks/src/hooks/useWallet.test.tsx
@@ -13,42 +13,47 @@ afterEach(() => {
   windowSpy.mockRestore();
 });
 
-test('should use the wallet', () => {
-  const wrapper = ({ children }) => (
-    <Provider network={1} infuraId={'abc123'}>
-      {children}
-    </Provider>
-  );
-  const { result } = renderHook(() => useWallet(), { wrapper });
+describe('useWallet tests', () => {
+  test('should use the wallet', () => {
+    const wrapper = ({ children }) => (
+      <Provider network={1} infuraId={'abc123'}>
+        {children}
+      </Provider>
+    );
+    const { result } = renderHook(() => useWallet(), { wrapper });
 
-  expect(result.current.connected).toBe(false);
-  expect(typeof result.current.connectWallet).toBe('function');
-});
+    expect(result.current.connected).toBe(false);
+    expect(typeof result.current.connectWallet).toBe('function');
+  });
 
-// initially is chainId undefined / requires network per the argument
-test('should be able to switch networks', () => {
-  const testNetwork = 2;
-  const wrapper = ({ children }) => (
-    <Provider network={testNetwork} infuraId={'abc123'}>
-      {children}
-    </Provider>
-  );
-  const { result } = renderHook(() => useWallet(), { wrapper });
+  // initially is chainId undefined / requires network per the argument
+  test('should be able to switch networks', () => {
+    const testNetwork = 2;
+    const wrapper = ({ children }) => (
+      <Provider network={testNetwork} infuraId={'abc123'}>
+        {children}
+      </Provider>
+    );
+    const { result } = renderHook(() => useWallet(), { wrapper });
 
-  let requestArgs;
+    let requestArgs;
 
-  windowSpy.mockImplementation(() => ({
-    ethereum: {
-      request: arg => {
-        requestArgs = arg;
+    // hijack the call to window.ethereum.request to capture those args
+    // this ensures that we have values in the requestArgs if/when request is called
+    // those are checked at the end
+    windowSpy.mockImplementation(() => ({
+      ethereum: {
+        request: arg => {
+          requestArgs = arg;
+        }
       }
-    }
-  }));
+    }));
 
-  result.current.switchToCorrectNetwork();
-  expect(windowSpy).toHaveBeenCalled();
-  expect(requestArgs).toStrictEqual({
-    method: 'wallet_switchEthereumChain',
-    params: [{ chainId: `0x${testNetwork}` }]
+    result.current.switchToCorrectNetwork();
+    expect(windowSpy).toHaveBeenCalled();
+    expect(requestArgs).toStrictEqual({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: `0x${testNetwork}` }]
+    });
   });
 });
