d0b2137fc4b195cf394dc412b2acf955a38ec375
Sweta Shaw <swt.shw94@gmail.com>
2021-12-27 11:30:29 +0530

    Removed convert functionality from the EtherInput component

--- packages/components/src/components/EtherInput/EtherInput.stories.tsx
+++ packages/components/src/components/EtherInput/EtherInput.stories.tsx
@@ -8,11 +8,13 @@ export default {
 
 const Component = ({ ...props }) => {
   const [value, setValue] = React.useState('');
+
   return (
     <>
-      <EtherInput value={value} {...props} />
+      <EtherInput value={value} unit='wei' onChange={(e) => setValue(e.target.value)} {...props} />
     </>
   );
 };
 
 export const Default = () => <Component />;
+export const Label = () => <Component label='Enter value in wei' />;
--- packages/components/src/components/EtherInput/EtherInput.tsx
+++ packages/components/src/components/EtherInput/EtherInput.tsx
@@ -1,50 +1,70 @@
-import {
-  Input,
-  InputGroup,
-  InputRightElement,
-  InputLeftAddon,
-  Button,
-  InputGroupProps,
-} from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftAddon, Text, InputGroupProps } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
 
 export interface EtherInputProps {
   /**
-   * The value to be converted
+   * Specifies the value of the EtherInput
    */
   value: string;
+
+  /**
+   * Input unit of the value entered in EtherInput
+   */
+  unit?: 'wei' | 'ether';
+
+  /**
+   * The label for EtherInput
+   */
+  label?: string;
+
+  /**
+   * Accepts a function that would be triggered when the value inside EtherInput changes.
+   */
+  onChange: (event: Event) => void;
 }
+/**
+ * A number input component with built-in conversion feature from wei <-> ether
+ */
+export const EtherInput: React.FC<EtherInputProps & InputGroupProps> = ({
+  value,
+  unit = 'ether',
+  label,
+  onChange,
+  ...props
+}) => {
+  // const [currentUnit, setCurrentUnit] = useState(unit);
 
-// A number input component with built-in conversion feature from wei <-> ether
-export const EtherInput: React.FC<EtherInputProps & InputGroupProps> = ({ value, ...props }) => {
-  const [currentUnit, setCurrentUnit] = useState('ether');
-  const [targetUnit, setTargetUnit] = useState('wei');
-  const [val, setVal] = useState(value);
+  /**
+   * The convert(unit) function converts the current value into the unit passed to the function
+   * @param unit
+   */
 
-  // The convert() function converts the current value into the unit selected by the user
-  const convert = () => {
-    if (targetUnit === 'wei') {
-      setCurrentUnit('wei');
-      setTargetUnit('ether');
-      setVal(ethers.utils.parseEther(val).toString());
-    } else if (targetUnit === 'ether') {
-      setCurrentUnit('ether');
-      setTargetUnit('wei');
-      let wei = ethers.BigNumber.from(val);
-      setVal(ethers.utils.formatEther(wei));
-    }
-  };
+  // const convert = (unit: string) => {
+  //   if (unit === 'wei') {
+  //     setValue(ethers.utils.parseEther(value).toString());
+  //   } else if (unit === 'ether') {
+  //     let wei = ethers.BigNumber.from(value);
+  //     setValue(ethers.utils.formatEther(wei));
+  //   }
+  // };
 
   return (
-    <InputGroup {...props}>
-      <InputLeftAddon children={currentUnit} />
-      <Input placeholder='Enter value' value={val} onChange={(e) => setVal(e.target.value)} />
-      <InputRightElement width='4.5rem'>
-        <Button h='1.75rem' size='sm' onClick={convert}>
-          To {targetUnit}
-        </Button>
-      </InputRightElement>
-    </InputGroup>
+    <>
+      {label ? <Text mb='8px'>{label}: </Text> : ''}
+      <InputGroup {...props}>
+        {/* <InputLeftAddon style={{ cursor: "pointer" }} children={currentUnit} onClick={() => {
+          if (currentUnit === "wei") {
+            convert("wei");
+            setCurrentUnit("ether");
+          } else {
+            convert("ether");
+            setCurrentUnit("wei");
+          }
+        }} /> */}
+        <InputLeftAddon children={unit} />
+        <Input placeholder={`Enter value in ${unit}`} value={value} onChange={onChange} />
+      </InputGroup>
+    </>
   );
 };
