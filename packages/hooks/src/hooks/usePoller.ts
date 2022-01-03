583e651b778d023a62f7d1f995cc15cb070d1f95
Brian Eter <brianeter.dev@gmail.com>
2022-01-04 05:52:09 +1100

    Added comments regarding usage of hook

--- packages/hooks/src/hooks/usePoller.ts
+++ packages/hooks/src/hooks/usePoller.ts
@@ -1,6 +1,18 @@
 import { useEffect, useRef } from "react";
 
 export const usePoller = (func: () => void, delay: number): void => {
+ 
+  /**
+ * Calls a function at a set interval. The function will also be called
+ * immediately.
+ *
+ * @param func The function to call at an interval
+ * @param delay The delay between each interval call
+ * @example
+ * const callback = () => console.log('test')
+ * usePoller(callback, 1000) // logs 'test' every second
+  */
+  
   const savedCbFunc = useRef(func);
 
   // Remember the latest fn.
