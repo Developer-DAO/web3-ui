4c52769916d1786e7973e7d7a9da92119639cfff
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2022-01-19 16:53:50 +0530

    Revert "Make test pass"
    This reverts commit a13e028436d960b903476afb512fd44e27a20cc9.

--- packages/components/src/components/TokenInput/TokenInput.test.tsx
+++ packages/components/src/components/TokenInput/TokenInput.test.tsx
@@ -2,23 +2,19 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { TokenInput } from '.';
 
-const Component = () => {
-  const [value, setValue] = React.useState('');
-  const [token, setToken] = React.useState('');
-
-  return (
-    <TokenInput
-      selectedToken={token}
-      onTokenChange={setToken}
-      value={value}
-      onValueChange={setValue}
-    />
-  );
-};
-
 describe('TokenInput', () => {
   it('renders correctly', () => {
-    const { container } = render(<Component />);
+    const [value, setValue] = React.useState('');
+    const [token, setToken] = React.useState('');
+    const { container } = render(
+      <TokenInput
+        selectedToken={token}
+        onTokenChange={setToken}
+        value={value}
+        onValueChange={setValue}
+        showDefualtTokens={true}
+      />
+    );
     expect(container).toBeInTheDocument();
   });
 });
