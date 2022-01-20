d6da2f65d8a45e19a6427835b4c7c0b958ab0703
Alex Plutta <alex.plutta@googlemail.com>
2022-01-20 22:38:57 +0100

    added some more testcases

--- packages/hooks/src/hooks/useTokenBalance.test.tsx
+++ packages/hooks/src/hooks/useTokenBalance.test.tsx
@@ -1,7 +1,6 @@
 import { renderHook } from '@testing-library/react-hooks';
 import React from 'react';
 import { useTokenBalance } from './useTokenBalance';
-//import { ERC20ABI } from "..";
 import { Web3Context } from '../Provider';
 import { BigNumber, ethers } from 'ethers';
 
@@ -11,6 +10,9 @@ jest.mock('ethers', () => ({
     ...jest.requireActual('ethers').ethers,
     Contract: class ContractStub {
       balanceOf(addr) {
+        //Throws an exception if address is not valid
+        //And causes the hook to fail
+        ethers.utils.getAddress(addr);
         return BigNumber.from(ethers.utils.parseEther('1.0'));
       }
       decimals() {
@@ -56,5 +58,104 @@ describe('useTokenBalanceTest', () => {
         BigNumber.from(ethers.utils.parseEther('1.0'))
       )
     );
+  });
+
+  test('tokenAddress is undefined - hook returns error; balance and decimals are undefined', async () => {
+    const wrapper = ({ children }) => (
+      <>
+        {/* pass an empty provider because we don't wan't to actually request an eth provider */}
+        {/* all web3 interaction should be mocked using the ContractSub above */}
+        {/* @ts-ignore */}
+        <Web3Context.Provider value={{ provider: {} }}>
+          {children}
+        </Web3Context.Provider>
+      </>
+    );
+
+    const { result, waitFor } = renderHook(
+      () =>
+        useTokenBalance({
+          //@ts-ignore
+          tokenAddress: undefined,
+          accountAddress: '0x503828976D22510aad0201ac7EC88293211D23Da',
+        }),
+      { wrapper }
+    );
+
+    await waitFor(() => expect(result.current.balance).toBeUndefined());
+
+    await waitFor(() => expect(result.current.loading).toEqual(false));
+    await waitFor(() => expect(result.current.error).toEqual(true));
+    await waitFor(() => expect(result.current.decimals).toBeUndefined());
+    await waitFor(() =>
+      expect(result.current.formattedBalance).toBeUndefined()
+    );
+    await waitFor(() =>
+      expect(result.current.balanceInBigNumber).toBeUndefined()
+    );
+  });
+
+  test('accountAddress is undefined - hook returns error; balance and decimals are undefined', async () => {
+    const wrapper = ({ children }) => (
+      <>
+        {/* @ts-ignore */}
+        <Web3Context.Provider value={{ provider: {} }}>
+          {children}
+        </Web3Context.Provider>
+      </>
+    );
+
+    const { result, waitFor } = renderHook(
+      () =>
+        useTokenBalance({
+          tokenAddress: '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f',
+          //@ts-ignore
+          accountAddress: undefined,
+        }),
+      { wrapper }
+    );
+
+    await waitFor(() => expect(result.current.balance).toBeUndefined());
+    await waitFor(() => expect(result.current.loading).toEqual(false));
+    await waitFor(() => expect(result.current.error).toEqual(true));
+    await waitFor(() => expect(result.current.decimals).toBeUndefined());
+    await waitFor(() =>
+      expect(result.current.formattedBalance).toBeUndefined()
+    );
+    await waitFor(() =>
+      expect(result.current.balanceInBigNumber).toBeUndefined()
+    );
+  });
+
+  test('accountAddress is invalid - hook returns error; balance and decimals are undefined', async () => {
+    const wrapper = ({ children }) => (
+      <>
+        {/* @ts-ignore */}
+        <Web3Context.Provider value={{ provider: {} }}>
+          {children}
+        </Web3Context.Provider>
+      </>
+    );
+
+    const { result, waitFor } = renderHook(
+      () =>
+        useTokenBalance({
+          tokenAddress: '0xde30da39c46104798bb5aa3fe8b9e0e1f348163f',
+          //@ts-ignore
+          accountAddress: 'bar',
+        }),
+      { wrapper }
+    );
+    await waitFor(() => expect(result.current.balance).toBeUndefined());
+    await waitFor(() => expect(result.current.loading).toEqual(false));
+    await waitFor(() => expect(result.current.error).toEqual(true));
+    await waitFor(() => expect(result.current.decimals).toBeUndefined());
+    await waitFor(() =>
+      expect(result.current.formattedBalance).toBeUndefined()
+    );
+    await waitFor(() =>
+      expect(result.current.balanceInBigNumber).toBeUndefined()
+    );
+    console.log(result.current);
   });
 });
