78e4cd192a6666c2c038431545da07b5ccafdafa
with-heart <with.heart+git@pm.me>
2022-01-26 11:06:04 -0500

    docs: generate bare nextra workspace

---
+++ docs/pages/_app.tsx
@@ -0,0 +1,8 @@
+import 'nextra-theme-docs/style.css';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
