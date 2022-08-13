2dbe8ccce439d79c73421810514d20d4f09843ff
e-roy <ericroupe@gmail.com>
2022-08-13 10:47:58 -0400

    docs:added pages for previous version

---
+++ apps/docs/pages/previous-version/web3-ui-core.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import { CopyBlock, dracula } from 'react-code-blocks';
+
+const Web3UIPreviousCorePage: NextPage = () => {
+  return (
+    <div>
+      <h1 className="my-2 text-center font-bold text-2xl text-gray-700 uppercase">
+        web3-ui/core 0.8.1
+      </h1>
+    </div>
+  );
+};
+
+export default Web3UIPreviousCorePage;
