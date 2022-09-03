3017e954ec00122dadb450d39d91711d61ede808
e-roy <ericroupe@gmail.com>
2022-09-03 11:19:17 -0400

    fix:playground error from dropdown merge

--- apps/playground/pages/web3-elements/index.tsx
+++ apps/playground/pages/web3-elements/index.tsx
@@ -4,16 +4,7 @@ import {
   Button,
   Badge,
   Checkbox,
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuCheckboxItem,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
+  Dropdown,
   Sheet,
   SheetTrigger,
   SheetContent,
@@ -94,39 +85,17 @@ const Web3ComponentsPage: NextPage = () => {
             weight={'semibold'}
             css={{ padding: '$4' }}
           >
-            DropdownMenu
+            Dropdown
           </Text>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button>Dropdown</Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuGroup>
-                <DropdownMenuItem>Item</DropdownMenuItem>
-                <DropdownMenuItem>Item</DropdownMenuItem>
-                <DropdownMenuItem>Item</DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuCheckboxItem>Item</DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem checked>
-                  Item
-                </DropdownMenuCheckboxItem>
-                <DropdownMenuCheckboxItem>Item</DropdownMenuCheckboxItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuLabel>Choose one</DropdownMenuLabel>
-                <DropdownMenuRadioGroup value="one">
-                  <DropdownMenuRadioItem value="one">
-                    Item
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="two">
-                    Item
-                  </DropdownMenuRadioItem>
-                  <DropdownMenuRadioItem value="three">
-                    Item
-                  </DropdownMenuRadioItem>
-                </DropdownMenuRadioGroup>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
+
+          <Dropdown
+            label={`Test label`}
+            placeHolder={`Test placeHolder`}
+            items={['Item 1', 'Item 2', 'Item 3']}
+            onSelect={(value) => {
+              console.log(value);
+            }}
+          />
         </div>
 
         {/* Sheet Element */}
